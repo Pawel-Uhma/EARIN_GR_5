1e638df9bc6873192e397e709b4e5f97398c126c
pawel uhma <paweluhma136@gmail.com>
2025-06-02 21:33:34 +0200

    config fix

--- PROJECT/src/config.py
+++ PROJECT/src/config.py
@@ -5,7 +5,7 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.abspath(os.path.join(THIS_DIR, os.pardir))
 
 DATA_DIR   = os.path.join(BASE_DIR, "data")
-RAW_DIR    = os.path.join(DATA_DIR, "processed")
+IMAGE_DIR    = os.path.join(DATA_DIR, "processed")
 CSV_FILE   = os.path.join(DATA_DIR, "train.csv")
 OUTPUT_DIR = os.path.join(BASE_DIR, "outputs")
 MODEL_DIR  = os.path.join(OUTPUT_DIR, "models")
