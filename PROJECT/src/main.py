5543ae5562d96539cbcc20c2d452eac724f0732f
pawel uhma <paweluhma136@gmail.com>
2025-06-02 22:44:52 +0200

    main fix

--- PROJECT/src/main.py
+++ PROJECT/src/main.py
@@ -3,11 +3,5 @@ from train import train
 from evaluate import evaluate
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Age Regression Training/Evaluation')
-    parser.add_argument('--mode', choices=['train','eval'], default='train')
-    parser.add_argument('--model_path', type=str, help='Path to model for evaluation')
-    args = parser.parse_args()
-    if args.mode == 'train':
-        train()
-    else:
-        evaluate(model_path=args.model_path)
+    train()
+    evaluate()
